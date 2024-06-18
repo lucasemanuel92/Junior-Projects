440392ff3002b166e8eb6a7e2de7da474f2c8904
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-06-18 11:49:01 -0300

    First part CRUD project

---
+++ Parte.py
@@ -0,0 +1,26 @@
+import mysql.connector;
+
+conexao = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'poasul2013',
+    database = 'library'
+)
+
+cursor = conexao.cursor()
+
+id_livro = 1
+titulo = "Volta ao Mundo em Oitenta dias"
+autor = "Júlio Verne"
+editora = "Melhoramentos"
+paginas = 182
+
+comando = f'INSERT INTO biblioteca (id_livro, titulo, autor, editora, paginas) VALUES ({id_livro}, {titulo}, {autor}, {editora}, {paginas})'
+cursor.execute(comando)
+conexao.commit() # Caso eu editar o bando de dados
+
+# resultado = cursor.fetchall() # Caso eu vá ler o banco de dados
+
+
+cursor.close()
+conexao.close
